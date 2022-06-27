3616f89fae958f6a8e166cce66adfbbeb15eba5b
godswill victor <godswillvictor5@gmail.com>
2022-06-27 16:39:15 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+    let x,y,t;
+    x=parseFloat(parseInt(prompt('enter first number')));
+y=parseFloat(parseInt(prompt('enter second number')));
+t=parseFloat(parseInt(prompt('enter third number')));
+let z=x+y+t;
+let a=x*y*t;
+let b=x-y-t;
+let c=x/y/t;
+alert ('the sum of the given number is:'+z);
+alert ('the product of the given number is:'+a);
+alert ('the difference of the given number is:'+b);
+alert ('the quotient of the given number is:'+c);
